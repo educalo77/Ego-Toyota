152c9dca2a3de32b8a7a7a8870d32cd8aa658023
educalo77 <educalo@outlook.com>
2021-01-15 16:51:34 -0300

    almost done

--- api/src/controllers/modelControllers.js
+++ api/src/controllers/modelControllers.js
@@ -34,9 +34,55 @@ const getOne = (id) => {
   });
 };
 
+// const editOne = ({ id, img, model, year, price, fullmodel, description, slogan, category }) => {
+//   return new Promise((resolve, reject) => {
+//     Models.update(id)
+//       .then((item) => {
+//         if (img) item.img = img;
+//         if (model) item.model = model;
+//         if (year) item.year = year;
+//         if (price) item.price = price;
+//         if (fullmodel) item.fullmodel = fullmodel;
+//         if (description) item.description = description;
+//         if (slogan) item.slogan = slogan;
+//         if (category) item.category = category;
+//         return item.save();
+//       })
+//       .then((item) => resolve(item))
+//       .catch((err) => reject(err));
+//   });
+// };
+
+
+const deleteOne = (id) => {
+  return new Promise((resolve, reject) => {
+    Models.destroy({
+      where: { id: id },
+    })
+      .then((removed) => {
+        resolve(removed);
+      })
+      .catch((err) => reject(err));
+  });   
+};
+
+const getAllCategory = (categoria) => {
+  return new Promise((resolve, reject) => {
+    Models.findAll({
+      where: { category: categoria },
+    })
+      .then((models) => {
+        resolve(models);
+      })
+      .catch((err) => reject(err));
+  });
+};
+
 
 module.exports = {
   createOne,
   getAll,
-  getOne
+  getOne,
+  deleteOne,
+  getAllCategory
 };
--- api/src/routes/modelRoutes.js
+++ api/src/routes/modelRoutes.js
@@ -3,7 +3,10 @@ const router = require("express").Router();
 const {
   createOne,
   getAll,
-  getOne
+  getOne,
+  editOne,
+  deleteOne,
+  getAllCategory
   
 } = require("../controllers/modelControllers");
 
@@ -22,6 +25,7 @@ router
 
   })
 
+
   router
     .route("/:id")
     .get((req, res) => {
@@ -29,7 +33,29 @@ router
     getOne(id)
       .then((model) => res.json(model).status(200))
       .catch((err) => res.status(404).json(err))
- 
+  })
+    .put((req, res) => {
+    const { id } = req.params;    
+    const { category } = req.body;
+    editOne(id, category)
+      .then((model) => res.json(model).status(201))
+      .catch((err) => res.status(400).json(err));
+  })
+    .delete((req, res) => {
+    const { id } = req.params;    
+      deleteOne(id)
+        .then((model) => res.json(model).status(201))
+      .catch((err) => res.status(400).json(err));
   })
 
-  module.exports = router;
+router
+  .route("/:category")
+  .post((req, res) => {
+    const category = req.params.category;
+    getAllCategory(category)
+      .then((models) => res.json(models).status(200))
+      .catch((err) => res.status(404).send(err));
+  })
+
+module.exports = router;
+  
